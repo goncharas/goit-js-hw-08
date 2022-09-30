743b260aa6d362f019853b4aa4bb48f516218a05
Andrii Honchar <goncharas1988@gmail.com>
2022-09-30 21:20:37 +0300

    update galery_01.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,32 @@
 // Add imports above this line
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
-console.log(galleryItems);
+
+const imageConteiner = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
+imageConteiner.insertAdjacentHTML('beforeend', imageMarkup);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}" data-caption="${description}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+imageConteiner.addEventListener('click', onClickImage);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+function onClickImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+}
