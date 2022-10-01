12d43a3066af98710c3d376a01c79972450f570a
Andrii Honchar <goncharas1988@gmail.com>
2022-10-01 21:43:12 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,7 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player
   .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
