1697f06c2b1ccf2467924d9d779cb46aa7678e76
Andrii Honchar <goncharas1988@gmail.com>
2022-10-01 21:37:33 +0300

    add video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player
+  .setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {});
+
+player.on('play', function (evt) {});
+
+player.on('timeupdate', throttle(onLisnerPlayer, 1000));
+
+function onLisnerPlayer(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
